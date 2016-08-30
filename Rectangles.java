2532bcea6b9116e907f814d7db0b8269135702d9
lewpiper <lewpiper@mac.com>
2016-08-29 23:00:38 -0700

    Rectangles Class
    Added rectangles class

---
+++ Rectangles.java
@@ -0,0 +1,19 @@
+import java.awt.Rectangle;
+
+/**
+* CSCI 114 online, Week 2 Rectangles homework
+*
+* @author Lew Piper III
+* @version 9/4/2016
+*/
+
+public class Rectangles
+{
+    public static void main(String[] args)
+    {
+
+        // you will write all of your new code here!
+
+    }
+}
+
